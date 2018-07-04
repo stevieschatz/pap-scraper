cca304c79d580227cdd387d2329de6acd68543b8
Stephen <stephen.schatzl@takeaway.com>
2018-07-04 17:05:10 +0200

    Replace express with koa

--- app.js
+++ app.js
@@ -1,28 +1,27 @@
-const express = require('express')
-const app = express()
+const Koa = require('koa')
+const Router = require('koa-router')
+
+const app = new Koa()
+const router = new Router()
+
 const port = process.env.port ||  3000
 const api = require('./lib/api.js')
 
-app.set('port', port)
+
+router.get('/health', (ctx) => {
+    res.send(200)
+})
+
+router.get('/clubs', async (ctx) => {
+    console.log('Clubs EP hit..')
+    let html = await api.getClubs()
+    console.log(html)
+
+})
+
 
 app.listen(port, () => {
     console.log('Listening on port 3000...')
 })
 
-app.get('/health', (req, res) => {
-    res.send(200)
-})
-
-app.get('/clubs', (req, res, err) => {
-
-    console.log('Clubs EP hit')
-
-    $html = api.getClubs()
-
-    res.send($html)
-
-
-
-})
-
-
+app.use(router.routes())
